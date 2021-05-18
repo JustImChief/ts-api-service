264c5f1be6fde89ea313cd4dbccd83925370fd97
justimchief <justimchief@gmail.com>
2021-05-18 16:04:18 +0300

    Init

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output:  {
     path:           path.resolve(__dirname, '_bundles'),
     filename:       '[name].js',
-    library:        'TSResponseServiceCli',
+    library:        'TSAPIService',
     umdNamedDefine: true,
   },
   resolve: {
